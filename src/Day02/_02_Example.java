794ea270539bae7b9a73c4e53089ad286b50f488
HaydarCanDurmaz <candrhydr@hotmail.com>
2024-04-15 16:09:19 +0300

    _02_Example

---
+++ src/Day02/_02_Example.java
@@ -0,0 +1,15 @@
+package Day02;
+
+public class _02_Example {
+    public static void main(String[] args) {
+        System.out.println(); // ekrana yazdırma komutu
+        System.out.println(); // Sistem in dışarıya yazdırma komutu
+
+        // kitap   okul.kutuphane.kitap
+        // kitap   sehir.kutuphane.kitap
+
+        // ismet.mehmet.Ayşe
+        // ahmet.zeynep.Ayşe
+
+    }
+}
