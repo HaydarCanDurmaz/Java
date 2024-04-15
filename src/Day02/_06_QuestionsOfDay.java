08d038cdb0597768656ba5a491ae8af6375a73b8
HaydarCanDurmaz <candrhydr@hotmail.com>
2024-04-15 16:23:27 +0300

    _06_QuestionsOfDay

---
+++ src/Day02/_06_QuestionsOfDay.java
@@ -0,0 +1,9 @@
+package Day02;
+
+public class _06_QuestionsOfDay {
+    public static void main(String[] args) {
+        //       Soru :   Ekrana      "Hello ", "World  \ /"        şeklinde yazınız
+
+        System.out.println("   \"Hello \", \"World \\ / \"  ");
+    }
+}
