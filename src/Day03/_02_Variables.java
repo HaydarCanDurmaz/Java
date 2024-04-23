25e5d933c566367ea200cb30be2672945b392231
HaydarCanDurmaz <candrhydr@hotmail.com>
2024-04-23 12:15:45 +0300

    _02_Variables

---
+++ src/Day03/_02_Variables.java
@@ -0,0 +1,12 @@
+package Day03;
+
+public class _02_Variables {
+    public static void main(String[] args) {
+        int sayi = 5;
+        int kisaKenar = 5;
+        int uzunKenar = 7;
+        int cevre;
+        cevre = kisaKenar + kisaKenar + uzunKenar + uzunKenar;
+        System.out.println("cevre = " + cevre);
+    }
+}
