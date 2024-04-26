7acf5ea802727e9932d2d46975246d04a28ee415
HaydarCanDurmaz <candrhydr@hotmail.com>
2024-04-26 16:51:38 +0300

    _05_Example

---
+++ src/Gun04/_05_Example.java
@@ -0,0 +1,19 @@
+package Gun04;
+
+public class _05_Example {
+    public static void main(String[] args) {
+// 3 farklı (String, char,int, boolean) tip de değişken tanımlayınız
+        // değer atayınız ve birleştirerek ekrana yazdırınız.
+        // İsmet Temur 5 A geçtiMi,   tamAd, sinif, sube, gectiMi
+
+        String  Name = "Zorbey" ;
+        String  SurName = "Durmaz" ;
+        int clasnumber =5;
+        char clasroom='A';
+        boolean pass=true;
+
+        System.out.println("FullName = " + Name+" "+SurName);
+        System.out.println("clasnumber = " + clasnumber+" "+clasroom);
+        System.out.println("pass or not = " + pass);
+    }
+}
