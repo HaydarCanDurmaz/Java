0d6aea15b37b74e22afc7bce8a0b58d294ee9e4c
HaydarCanDurmaz <candrhydr@hotmail.com>
2024-04-29 15:09:24 +0300

    _03_Example

---
+++ src/Day05/_03_Example.java
@@ -0,0 +1,17 @@
+package Day05;
+
+import java.util.Scanner;
+
+public class _03_Example {
+    public static void main(String[] args) {
+        // Kullanıcıdan Adını ve soyadını alarak ekrana yazdırınız
+
+        Scanner oku = new Scanner(System.in);// okuma işlemini yapacak olan değişken tanımlandı
+
+        System.out.print("Adınız ve soyadınız =");// ekrana neyin girileceğini yazdık bilgilendirme
+        String adSoyad= oku.nextLine();//okuma işlemi bu noktada yapılıyor
+
+        System.out.println("adSoyad = " + adSoyad);// verinin gerçekten okunabildiğini
+        // tekrar ekrana yazarak kontrol ettim.
+    }
+}
