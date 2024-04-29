cdf5af09ddfe7c022c46a8553145c9a71c636e99
HaydarCanDurmaz <candrhydr@hotmail.com>
2024-04-29 16:03:38 +0300

    _08_Example

---
+++ src/Day05/_08_Example.java
@@ -0,0 +1,15 @@
+package Day05;
+
+import java.util.Scanner;
+
+public class _08_Example {
+    public static void main(String[] args) {
+
+        Scanner oku=new Scanner(System.in);
+
+        System.out.print("bir DOUBLE değer giriniz=");
+        double deger= oku.nextDouble();
+
+        System.out.println("deger = " + deger);
+    }
+}
