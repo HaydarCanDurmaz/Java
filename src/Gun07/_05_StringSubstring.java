9695d3f690ec5e06af46975faad35bb5b8fab3dc
HaydarCanDurmaz <candrhydr@hotmail.com>
2024-05-02 11:42:57 +0300

    _05_StringSubstring

---
+++ src/Gun07/_05_StringSubstring.java
@@ -0,0 +1,22 @@
+package Gun07;
+
+public class _05_StringSubstring {
+    public static void main(String[] args) {
+
+        // String in belli bir bölümünü alma işlemi
+
+//                 012345678911121314
+        String s1="Hello Everyone";
+
+
+        String part1=s1.substring(3,9);// 3 nolu indexten 9 nolu indezse kadar
+        System.out.println("part1 = " + part1);
+
+        String part2=s1.substring(6,14);
+        System.out.println("part2 = " + part2);
+
+        String part3=s1.substring(6); // verilen den başlayıp sonuna kadar alır
+        System.out.println("part3 = " + part3);
+
+    }
+}
