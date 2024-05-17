c22994c0914146aca77ae87612d09d96e01ee9d8
HaydarCanDurmaz <candrhydr@hotmail.com>
2024-05-17 17:12:55 +0300

    HomeWorkJava2

---
+++ src/HomeWorkJava/HomeWorkJava2.java
@@ -0,0 +1,101 @@
+package HomeWorkJava;
+
+public class HomeWorkJava2 {
+    public static void main(String[] args) {
+        /*
+
+         */
+
+        // 1- Değeri 'true' olan bir boolean oluşturunuz. Boolean'ı yazdırınız.
+
+        Boolean wearesdet=true;
+        System.out.println("wearesdet = " + wearesdet);
+
+        // 2 -Değeri 10.01 olan bir double oluşturunuz. Double'ı yazdırınız.
+        Double count=10.01;
+        System.out.println("count = " + count);
+
+       // 3- Değer i 'false' olan bir boolean oluşturunuz. Boolean'ı yazdırınız.
+        boolean werentsdt=false;
+        System.out.println("werentsdt = " + werentsdt);
+
+       // 4- Değeri 5 olan bir byte oluşturun. Bu byte'ı yazdırın.
+        byte byt=5;
+        System.out.println("byt = " + byt);
+
+        // 5- Değeri -4 olan bir byte oluşturun. Bu byte'ı yazdırın.
+        byte bbyt=-4;
+        System.out.println("byt = " + bbyt);
+
+       // 6-  Değeri a olan bir char oluşturun. Bu char'ı yazdırın.
+        char chr='a';
+        System.out.println("chr = " + chr);
+
+        // 7- Değeri z olan bir char oluşturun. Bu char'ı yazdırın.
+        char chhr='z';
+        System.out.println("chr = " + chhr);
+
+        // 8- Değeri & olan bir char oluşturun. Bu char'ı yazdırın.
+        char chrr='&';
+        System.out.println("chr = " + chrr);
+
+        // 9- Değeri 120 olan bir double oluşturunuz. Double' ı yazdırınız.
+        double duble=120;
+        System.out.println("duble = " + duble);
+
+        // 10- Değeri 120.005 olan bir double oluşturunuz.Double'ı yazdırınız.
+        double duble1=120.005;
+        System.out.println("duble = " + duble1);
+
+        // 11- Değeri 550.24  olan bir double oluşturunuz.Double'ı yazdırınız.
+        double duble2=550.24;
+        System.out.println("duble = " + duble2);
+
+        // 12- Değeri 121.005f olan bir float oluşturunuz.Float'ı yazdırınız.
+        float flt=121.005f;
+        System.out.println("flt = " + flt);
+
+        // 13- Değeri 121.005f olan bir float oluşturunuz.Float'ı yazdırınız.
+        float flt1=121.005f;
+        System.out.println("flt = " + flt1);
+
+        // 14-Değeri 123.3365f olan bir float oluşturunuz.Float'ı yazdırınız.
+        float flt2=123.3365f;
+        System.out.println("flt = " + flt2);
+
+        // 16- Değeri 3.55f olan bir float oluşturunuz.Float'ı yazdırınız.
+        float flt3=3.55f;
+        System.out.println("flt = " + flt3);
+
+        // 17- Değeri 15 olan bir int oluşturun.Int'i yazdırın.
+        int count1=15;
+        System.out.println("count1 = " + count1);
+
+        // 18- Değeri 100 olan bir int oluştur.Ve bu int 'i yazdır.
+        int count2=100;
+        System.out.println("count1 = " + count2);
+
+        // 19- Değeri 120 olan bir int oluşturın.Int'i yazdırın.
+        int count3=120;
+        System.out.println("count1 = " + count3);
+
+        // 20- Değeri -6500 olan bir int oluşturunuz.Int'i yazdırınız.
+        int count4=-6500;
+        System.out.println("count1 = " + count4);
+
+        // 21- Değeri 12 olan bir short oluşturunuz.Short'u yazdırınız.
+        short srt=12;
+        System.out.println("srt = " + srt);
+
+        // 22- Değeri 23 olan bir short oluşturunuz.Short'u yazdırınız.
+        short srt1=23;
+        System.out.println("srt = " + srt1);
+
+        // 23- Değeri -100 olan bir short oluşturun.Short'u yazdırın.
+        short srt2=-100;
+        System.out.println("srt = " + srt2);
+
+        String given="Z O R B E Y-D U R M A Z";
+        System.out.println("Sonuç"+given.replaceAll("R",""));
+    }
+}
