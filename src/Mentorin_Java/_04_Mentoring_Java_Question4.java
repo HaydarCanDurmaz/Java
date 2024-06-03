56dce38800bd0c0558da276407b782963bf5d4ee
HaydarCanDurmaz <candrhydr@hotmail.com>
2024-06-03 14:28:04 +0300

    _04_Mentoring_Java_Question4

---
+++ src/Mentorin_Java/_04_Mentoring_Java_Question4.java
@@ -0,0 +1,22 @@
+package Mentorin_Java;
+
+import java.util.Scanner;
+
+public class _04_Mentoring_Java_Question4 {
+    public static void main(String[] args) {
+        // Örnek Soru : Trafikte halindeki kullanıcıdan trafik ısıklarından hangisinin
+        // yandıgını bilgisini alıp yönlenme yapan program yazınız.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Which Colours light now =");
+        String light = scanner.nextLine();
+
+        if (light.equalsIgnoreCase("Red")) {
+            System.out.println("Stop");
+        } else if (light.equalsIgnoreCase("Yellow")) {
+            System.out.println("get ready");
+        } else if (light.equalsIgnoreCase("Green")) {
+            System.out.println("Go");
+        }
+    }
+}
